cb839eda014267368752a5061fc01fa04d5eff81
TaurMorn <taurmorn@gmail.com>
2023-02-24 20:22:51 +0200

    websocket from client

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,11 +1,25 @@
 let hiddenRoomNumber = document.getElementById("message-room-number");
 let roomForm = document.getElementById("room-form");
 let messageForm = document.getElementById("message-form");
-
+let textArea = document.getElementById ("text-input");
+let websocket = null;
 
 window.addEventListener("DOMContentLoaded", (_) => {
     if (hiddenRoomNumber.value) {
+        let protocol = location.protocol === "http:" ? "ws:" : "wss:" ;
+        websocket = new WebSocket(protocol + "//" + window.location.host + "/websocket");
         roomForm.hidden = true;
         messageForm.hidden = false;
+
+        websocket.addEventListener("open", (event) => {
+            let msg =  JSON.stringify({
+                RoomNumber: hiddenRoomNumber.value,
+                UserName: nickName.value,
+                Message: "Hello, blah-blah-blah"
+              });
+            websocket.send(msg);
+        });
+
+        textArea.focus();
     }
 } );
